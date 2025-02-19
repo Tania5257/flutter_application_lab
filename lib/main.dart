18b200a31a17cc8ba134195d5b55315431cb54ba
Tania5257 <tania15-5257@diu.edu.bd>
2025-02-19 12:49:45 +0600

    Practicetask

--- lib/main.dart
+++ lib/main.dart
@@ -5,37 +5,93 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter UI',
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Flutter UI"),
+          title: Text('Product'),
         ),
-        body: Container(
-          // height: double.infinity,
-          width: double.infinity,
-          child: SingleChildScrollView(
-            scrollDirection: Axis.vertical,
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              crossAxisAlignment: CrossAxisAlignment.center,
-           
-            children: List.generate(
-              100,
-              (index) => Icon(
-                Icons.radar,
-                color: Colors.blue,
-                size: index.toDouble(),
-              ),
-            ), 
-            ) 
-          ),
+        body: Center(
+          child: Product(),
         ),
       ),
     );
   }
 }
+
+class Product extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(15.0),
+      width: 250.0,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10),
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.white,
+            spreadRadius: 3,
+            blurRadius: 5,
+            offset: Offset(0, 3),
+          ),
+        ],
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Container(
+                padding: EdgeInsets.all(4.0),
+                color: Colors.blue,
+                child: Text(
+                  '50% OFF',
+                  style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+                ),
+              ),
+              Icon(Icons.favorite_border),
+            ],
+          ),
+          SizedBox(height: 10.0),
+          
+          Center(
+            child: Image.network(
+             'asset\abc.png'
+              height: 100.0,
+            ),
+          ),
+          SizedBox(height: 10.0),
+         
+          Text(
+            'Shoes',
+            style: TextStyle(fontSize: 15.0, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 5.0),
+         
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Text(
+                '45,000',
+                style: TextStyle(fontSize: 17.0, fontWeight: FontWeight.bold, color: Colors.green),
+              ),
+              Text(
+                '55,000',
+                style: TextStyle(
+                  fontSize: 14.0,
+                  decoration: TextDecoration.lineThrough,
+                  color: Colors.grey,
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
+
